c8e180af58a1981537e0fea5053613d87fe12c2c
Naveen Kumar Sangi <devteam.sae@gmail.com>
2016-05-27 01:33:43 +0530

    Linked the billing operation with the inventory

--- js/main.js
+++ js/main.js
@@ -64,6 +64,32 @@ jQuery(function($) {'use strict',
 	    scrollThreshold: 0.3,
 	    filter: ':not(.no-scroll)'
 	});
+                    
+    $(function(){
+
+        $("#typed").typed({
+            // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
+            stringsElement: $('#typed-strings'),
+            typeSpeed: 30,
+            backDelay: 2000,
+            loop: true,
+            contentType: 'html', // or text
+            // defaults to false for infinite loop
+            loopCount: false, 
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+
+        $(".reset").click(function(){
+            $("#typed").typed('reset');
+        });
+
+    });
+        
+        function newTyped(){ /* A new typed object */ }
+
+        function foo(){ console.log("Callback"); }
+
 
 });
 
@@ -145,6 +171,7 @@ jQuery(function($) {'use strict',
 
 	map.setStyle("map_style");
 }());
+
 
 
 
