c2171490d23904a4ee1599e66adde1ae7b79bccf
Naveen Kumar Sangi <devteam.sae@gmail.com>
2016-05-27 15:08:35 +0530

    Added autotyper

--- js/main.js
+++ js/main.js
@@ -170,6 +170,31 @@ jQuery(function($) {'use strict',
 	});
 
 	map.setStyle("map_style");
+    
+    $(function(){
+
+        $("#typed").typed({
+            // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
+            stringsElement: $('#typed-strings'),
+            typeSpeed: 30,
+            backDelay: 500,
+            loop: true,
+            contentType: 'html', // or text
+            // defaults to false for infinite loop
+            loopCount: false,
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+
+        $(".reset").click(function(){
+            $("#typed").typed('reset');
+        });
+
+    });
+
+    function newTyped(){ /* A new typed object */ }
+
+    function foo(){ console.log("Callback"); }
 }());
 
 
